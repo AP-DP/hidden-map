3e551a409a5d46e2c9c2ad5f09a83acbd2866c56
APDP <ap_duplessis@live.com>
2023-07-27 22:27:47 -0600

    Connected MVC components

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@
  * Instantiate classes using p5 here
  */
 
-let sketch = new HiddenMap("main-canvas");
+let model = new Model();
+let controller = new Controller(model);
+
+let sketch = new HiddenMap("main-canvas", model, controller);
 sketch.draw();
+
+model.addListener(sketch);
