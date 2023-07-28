6e780c805ba27864c2a465cbbc27d2b7ed430558
APDP <ap_duplessis@live.com>
2023-07-27 22:42:28 -0600

    Manage new-user event and record user IDs

--- socket.js
+++ socket.js
@@ -1,5 +1,8 @@
 // Handle all socket logic here
 
+let users = {};
+let lastUsedID = 1;
+
 /**
  * Set up io object
  * @param {Object} HTTP Server
@@ -13,11 +16,17 @@ function initIO(server) {
     //Handle connection
     io.on('connection', (socket) => {
         console.log('a user connected: ' + socket.id);
+        io.emit("new-user", socket.id);
+        for (const [key, value] of Object.entries(users)) {
+            console.log(key);
+            socket.emit("new-user", key);
+        }
+        users[socket.id] = lastUsedID;
+        lastUsedID++;
         socket.emit("test-event");
         socket.on('mouse', (data) => {
             socket.broadcast.emit('mouse', data);
         })
-    
     });
 
     return io;
