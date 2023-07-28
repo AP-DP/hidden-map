c4c9e7904e4e37acbd1e637570dd28d89bc881dd
APDP <ap_duplessis@live.com>
2023-07-27 22:36:14 -0600

    Moved socket functions inside controller. Event "new-user" tracks the user id the first time it is called, and collaborator ids on subsequent calls.

--- src/controller.js
+++ src/controller.js
@@ -1,18 +1,32 @@
-// Handle all multiuser actions
-const socket = io('ws://localhost:8080');
-
-function getSocket() {
-    return socket;
-}
-
-socket.on("test-event", () => {
-    console.log("Testing server to client event");
-})
-
-socket.on("mouse", (data) => {
-    sketch.collabDraw(data);
-})
-
-function sendMessage(msgName, msgBody) {
-    socket.emit(msgName, msgBody);
+/**
+ * Controller manages socket io events
+ */
+class Controller {
+    constructor(model) {
+        this.socket = io('ws://localhost:8080');
+        let socket = this.socket;
+        socket.on("new-user", (id) => {
+            let user = model.getUser();
+            console.log("Current user: " + user);
+            if (user == "") {
+                console.log("setting user");
+                model.setUser(id);
+            }
+            else {
+                console.log("adding collaborator");
+                model.addCollaborator(id);
+            }
+        })
+        socket.on("test-event", () => {
+            console.log("Testing server to client event");
+        })
+        
+        socket.on("mouse", (data) => {
+            sketch.collabDraw(data);
+        })
+    }
+    sendMessage(msgName, msgBody) {
+        let socket = this.socket;
+        socket.emit(msgName, msgBody);
+    }
 }
